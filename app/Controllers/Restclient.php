5a4bdab7f763aa59639e8024baabdec5ec328243
Rdn-M <muhammadreadone96@gmail.com>
2022-03-19 00:32:08 +0700

    fixing error

--- app/Controllers/Restclient.php
+++ app/Controllers/Restclient.php
@@ -1,17 +1,17 @@
 <?php namespace App\Controllers;
 
-class Home extends BaseController
+class Restclient extends BaseController
 {
 	public function index()
 	{
 		$client = \Config\Services::curlrequest();
 		$token  = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IlJpZHdhbiIsImVtYWlsIjoiUmlkd2FuQGdtYWlsLmNvbSIsImlhdCI6MTY0NzYyMjU2OCwiZXhwIjoxNjQ3NjI2MTY4fQ.LRzc4jqLIbcPGhfbvNnzKboHxB2gWWXMc3lhDLqxolU";
-		$url    ="http://localhost/ci4-rest-api/public/stocks";
+		$url    ="http://localhost/Example-Api/public/stocks";
 		$headers = [
 			'Authorization' => 'Bearer ' . $token
 		];
 		$response = $client->request('GET',$url,['headers'=>$headers]);
-		print_r($response);
+		echo $response->getBody();
 	}
 
 	//--------------------------------------------------------------------
